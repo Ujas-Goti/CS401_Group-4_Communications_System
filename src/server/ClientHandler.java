a3a35f92e1f77a8d8ba5b4279c256fe5636cecd7
W2CH <modeb993@gmail.com>
2025-12-02 11:43:52 -0800

    Refactored Server and Client handler
    1. Made sure to pass proper arguements to the handler constructor
    2. Declared a authentication object for every user that logs in
    3. Server should now handle recieving and handling user credentials from the auth GUI.
    4. Specified the package that all the files reside in.

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,3 +1,4 @@
+package server;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -7,13 +8,15 @@ import java.net.Socket;
 public class ClientHandler implements Runnable {
 
     private final Socket clientSocket;
+    private final Authentication auth;
     private ObjectInputStream inputStream;
     private ObjectOutputStream outputStream;
     private boolean isLoggedIn = false;
 
     // Constructor saves the client's socket
-    public ClientHandler(Socket clientSocket) {
+    public ClientHandler(Socket clientSocket, Authentication auth) {
         this.clientSocket = clientSocket;
+        this.auth = auth;
     }
 
     @Override
@@ -28,11 +31,57 @@ public class ClientHandler implements Runnable {
 
             // Keep listening for objects that are getting passed in
             while (true) {
+                
+                // For now just assume that we are reciving the credentials
+                // as two strings.
+                
+                // 1. get username
+            	
+            	// Check if the object that was passed is a String
+                Object username = inputStream.readObject();
+                if (!(username instanceof String)) {
+                    System.out.println("Expected username String, got: " + username);
+                    break;
+                }
+                String username = (String) username;
+                
+                // 2. get password
+                
+                // Check if the object that was passed is a String
+                Object password = inputStream.readObject();
+                if(!(password instanceof String)) {
+                	System.out.println("Expected username String, got: " + password);
+                    break;
+                }
+                
+                String password = (String) password;
+                
+                System.out.println("Ok, just received login attempt from username: " + username);
 
-                Object receivedObject = inputStream.readObject();
+                // 3. pass username and password to authentication
+                User loggedInUser = auth.validateCredentials(username, password);
+                	
+                // This will either send the client a User object if user logged in successfully otherwise null.
+                outputStream.writeObject(loggedInUser);
+                outputStream.flush();
+                outputStream.reset();
+                
+                break;
+                
+                // 4. attempt to send back a user object to the client
+                
+              
+                
 
                 // This is where I plan to write the route system for our APIS
                 
+                	
+                // Cases for when the object is a user
+                //     1. Authenitcating
+                
+                // Cases for when the object is a Message
+                //     1. Ask chat manager to either perserve
+                //     2. Route
                 }
               
          }
