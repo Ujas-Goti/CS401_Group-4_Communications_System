048c686ee63a401681dae7ccf3b1cb2dee7b79d1
W2CH <modeb993@gmail.com>
2025-12-01 20:26:15 -0800

    Update User.java
    1. Assigning IDs to username since they are unique.
    2. Defined toString method.

--- src/common/User.java
+++ src/common/User.java
@@ -10,29 +10,39 @@ public class User {
 		this.username = username;
 		this.password = password;
 		this.role = role;
+		this.userID = username;
 	}
 	
 	public String getUserID() {
-		return userID;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public UserRole getRole() {
-		return role;
-	}
-	public OnlineStatus getStatus() {
-		return status;
-	}
-	public void setStatus(OnlineStatus status) {
-		this.status = status;
-	}
-	public void setUserName(String username) {
-		this.username = username;
-	}
-	
-	// Need to ask team what the format will be
-	public toString() {
-		return "";
-	}
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public OnlineStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OnlineStatus status) {
+        this.status = status;
+    }
+
+    public void setUserName(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
 }
